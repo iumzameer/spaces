d5085166ce62268528a313bf67aa67ea516afff4
Zameer <ibrahim.zameer@ium.edu.mv>
2024-10-12 23:45:18 +0500

    Sync

--- app/Livewire/user/Wizard.php
+++ app/Livewire/user/Wizard.php
@@ -3,26 +3,48 @@
 namespace App\Livewire\User;
 
 use Livewire\Component;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Company;
+use App\Models\Location;
+use App\Models\Etype;
+use App\Models\Tier;
+use App\Models\Item;
 
 class Wizard extends Component
 {
     public $stepDescription = "Information";
     public $buttonText = "Next";
     public $step = 1;
+    public $rType = null;
     public $companySection = false;
     public $company = [];
     public $companyId = null;
+    public $locationId = null;
+    public $etypeId = null;
+    public $tierId = null;
+    public $itemId = null;
     public $infoSection = true;
     public $locationSection = false;
     public $confirmSection = false;
     public $showItem = false;
+    public $showDuration = false;
     public $locationDetails = false;
+    public $showLocationError = false;
+    public $locations, $etypes, $tiers, $from, $to, $qty = 1;
+    public $rlocations = [];
+    public $ritems = [];
+    public $items;
+    public $msg;
 
     public function mount()
     {
         $this->company = Company::first();
+        $this->locations = Location::all();
+        $this->etypes = Etype::all();
+        $this->tiers = Tier::all();
+        $this->items = Item::all();
     }
 
     public function render()
@@ -42,6 +64,39 @@ class Wizard extends Component
             $this->companyId = null;
             $this->companySection = false; 
         }
+
+    }
+
+    public function updatedlocationId()
+    {
+        $this->location = Location::find($this->locationId);
+        $this->tiers = $this->location->tiers;
+    }
+
+    public function updatedFrom()
+    {
+        $this->checkDuration();
+    }
+
+    public function updatedTo()
+    {
+        $this->checkDuration();
+    }
+
+    public function checkDuration()
+    {
+        if(!is_null($this->from)||($this->from == "")||!is_null($this->to)||($this->to == ""))
+        {
+            $this->showDuration = false;
+        }
+        if($this->from == $this->to)
+        {
+            $this->showDuration = true;
+        }
+        else
+        {
+            $this->showDuration = false;
+        }
     }
 
     public function nextStep()
@@ -59,6 +114,7 @@ class Wizard extends Component
                 $this->stepDescription = "Agreement";
                 $this->infoSection = false;
                 $this->locationSection = false;
+                $this->locationDetails = false;
                 $this->confirmSection = true;
                 $this->buttonText = "Confirm Reservation";
                 $this->step = 3;
@@ -77,11 +133,76 @@ class Wizard extends Component
 
     public function addLocation()
     {
-        $this->locationDetails = true;
+        $validator = Validator::make(
+        [
+            'locationId' => $this->locationId,
+            'from' => $this->from,
+            'to' => $this->to,
+            'etypeId' => $this->etypeId,
+            'tierId' => $this->tierId,
+        ],
+        [
+            'locationId' => 'required|exists:locations,id',
+            'from' => 'required',
+            'to' => 'required',
+            'etypeId' => 'sometimes|required',
+        ],
+        [
+            'locationId.required' => 'Location is required!',
+            'locationId.exists' => 'Location is invalid!',
+            'from.required' => 'From Date is required!',
+            'to.required' => 'To Date is required!',
+            'etypeId.required' => 'Purpose is required!',
+        ]);
+
+        if ($validator->fails())
+        {
+            $this->msg = $validator->errors()->first();
+            $this->showLocationError = true;
+        }
+        else
+        {
+            $this->showLocationError = false;
+            $location = Location::find($this->locationId);
+            $etype = Etype::find($this->etypeId);
+
+            if(Carbon::parse($this->from)->diff(Carbon::parse($this->to))->days < 1)
+            {
+                $tier = Tier::find($this->tierId);
+            }
+            else
+            {
+                $tier = Carbon::parse($this->from)->diff(Carbon::parse($this->to))->days;
+            }    
+
+            $rlocation = ["location" => $location, "etype" => $etype, "tier" => $tier];
+
+            array_push($this->rlocations, $rlocation);
+
+            $this->locationDetails = true;
+
+            $this->locationId = null;
+            $this->from = null;
+            $this->to = null;
+            $this->etypeId = null;
+            $this->tierId = null;
+        }
     }
 
     public function addItem()
     {
-        $this->showItem = true;
+        $item = Item::find($this->itemId);
+        $item->qty = $this->qty;
+
+        array_push($this->ritems, $item);
+    }
+
+    public function updatedrType()
+    {
+        if(($this->rType == 2)||($this->rType == ""))
+        {
+            $this->companyId == null;
+            $this->companySection = false;
+        }
     }
 }
--- resources/views/livewire/user/wizard.blade.php
+++ resources/views/livewire/user/wizard.blade.php
@@ -23,12 +23,19 @@
             </div>
             <div class="p-6 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 mt-5">
                 <div class="mb-4">
-                    <select wire:model.lazy="companyId" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
+                    <select wire:model.lazy="rType" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
+                      <option value="">Select Reservation Type</option>
+                      <option value="2">Individual</option>
+                      <option value="1">Company/Institute</option>
+                    </select>
+                    @if((!is_null($rType))&&($rType == "1"))
+                    <select wire:model.lazy="companyId" class="ml-10 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
                       <option value="">Select Company/Institute</option>
                       @foreach(\Auth::user()->companies as $companyx)
                         <option value="{{ $companyx->id }}">{{ $companyx->name }}</option>
                       @endforeach
                     </select>
+                    @endif
                 </div>
                 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 {{ $companySection == false ? 'hidden' : 'block' }}">
                    <div>
@@ -44,86 +51,103 @@
         </section>
         <section class="{{ $locationSection == false ? 'hidden' : 'block' }}">
             <div class="p-6 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 mt-5">
-                <div class="grid grid-cols-4 gap-6">
-                    <select wire:model="location" class="w-full shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
-                      <option value="">Select Location</option>
-                      <option value="company1">Location A</option>
-                      <option value="company2">Location B</option>
-                    </select>
-                    <select wire:model="purpose" class="w-full ml-5 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
-                      <option value="">Select Purpose</option>
-                      <option value="company1">Purpose 1</option>
-                      <option value="company2">Purpose 2</option>
+                <div class="grid grid-cols-3 gap-6">
+                    <select wire:model.lazy="locationId" class="w-full shadow appearance-none border rounded mt-7 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
+                        <option value="">Select Location</option>
+                        @foreach($locations as $location)
+                            <option value="{{$location->id}}">{{ $location->description }} ({{$location->capacity}} pax)</option>
+                        @endforeach
                     </select>
                     <div>
                         <label class="block text-gray-700 text-sm font-bold mb-2">From</label>
-                            <input type="date" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" required wire:model = "nid">
+                            <input type="date" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" required wire:model.lazy="from" min="{{\Carbon\Carbon::now()->addDay()->format('Y-m-d')}}">
                     </div>
                     <div>
-                        <label class="block text-gray-700 text-sm font-bold mb-2">From</label>
-                            <input type="date" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" required wire:model = "nid">
+                        <label class="block text-gray-700 text-sm font-bold mb-2">To</label>
+                            @php
+                                if(is_null($from) || ($from == ""))
+                                {
+                                    $to_min = \Carbon\Carbon::now()->addDay()->format('Y-m-d');
+                                }
+                                else
+                                {
+                                    $to_min = $from;
+                                }
+                            @endphp
+                            <input type="date" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" required wire:model.lazy="to" min="{{$to_min}}">
                     </div>
-                    <select wire:model="company" class="ml-5 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
-                      <option value="">Select Duration</option>
-                      <option value="company1">8 Hrs</option>
-                      <option value="company2">12 Hrs</option>
-                      <option value="company2">24 Hrs</option>
+                    <select wire:model="etypeId" class="w-full shadow appearance-none border rounded mt-7 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
+                        <option value="">Select Purpose</option>
+                        @foreach($etypes as $etype)
+                            <option value="{{$etype->id}}">{{$etype->description}}</option>
+                        @endforeach
                     </select>
-                    <div>
-                    <button class="w-1/6 py-3 bg-teal-500 text-white rounded hover:bg-teal-600" wire:click="addLocation">Add Location</button>
-                </div>
-            </div>
-            <div class="p-6 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 mt-2 {{ $locationDetails == false ? 'hidden' : 'block' }}">
-                <div class="grid grid-cols-1 gap-6 sm:grid-cols-3">
-                    <div class="border-r pr-10 text-left">
-                        <p class="text-lg"><span class="font-semibold">Location:</span> Location A</p>
-                        <p class="text-lg"><span class="font-semibold">Purpose:</span> Purpose 1</p>
-                        <p class="text-lg"><span class="font-semibold">Duration:</span> 8 Hrs</p>
+                    <select wire:model="tierId" class="w-full shadow appearance-none border rounded mt-7 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5 {{ $showDuration == false ? 'opacity-0 -z-index-10' : 'opacity-100 z-index-10' }}">
+                        <option value="">Select Duration</option>
+                        @foreach($tiers as $tier)
+                            <option value="{{ $tier->id }}">{{$tier->hrs}} Hrs</option>
+                        @endforeach
+                    </select>
+                    <div class="flex justify-end">
+                        <button class="w-full mt-7 py-3 bg-lime-500 text-white rounded hover:bg-lime-600" wire:click="addLocation">Add Location</button>
                     </div>
-                    <div class="col-span-2">
-                        <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-                            <thead class="text-xs text-gray-700 uppercase border-b">
-                                <tr>
-                                    <th scope="col" class="px-6 py-3">
-                                        Item
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Qty
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Duration
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="{{ $showItem == false ? 'opacity-0' : 'opacity-100' }}">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                        Item 1
-                                    </th>
-                                    <td class="px-6 py-4">
-                                        1
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        1 day(s)
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>    
                 </div>
-                <div class="flex justify-between mt-10">
-                    <select wire:model="item" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
-                      <option value="">Select Item</option>
-                      <option value="company1">Item 1</option>
-                      <option value="company2">Item 2</option>
-                      <option value="company2">Item 3</option>
-                    </select>
-                    <input type="number" placeholder="Quantity" wire:model="qty" class="ml-5 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
-                    <input type="number" placeholder="Duration (Days)" wire:model="duration" class="ml-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/5">
-                    <button class="w-1/6 py-3 bg-lime-500 text-white rounded hover:bg-lime-600" wire:click="addItem">Add Item</button>
-                </div>
+                <div class="mt-5 col-span-5 bg-red-100 w-full rounded px-2 py-1 text-sm text-red-800 {{ $showLocationError === true ? 'block' : 'hidden' }}">{{$msg}}</div>
             </div>
         </section>
+        @foreach($rlocations as $rlocation)
+        <div class="p-6 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 mt-5 {{ $locationDetails == false ? 'hidden' : 'block' }}">
+            <div class="grid grid-cols-1 gap-6 sm:grid-cols-3">
+                <div class="border-r pr-10 text-left">
+                    <p class="text-lg"><span class="font-semibold">Location:</span> {{$rlocation["location"]->description}}</p>
+                    <p class="text-lg"><span class="font-semibold">Purpose:</span> {{$rlocation["etype"]->description}}</p>
+                    <p class="text-lg"><span class="font-semibold">Duration:</span> 
+                        @if(!isset($rlocation["tier"]->hrs))
+                            {{$rlocation["tier"]}} Day(s)
+                        @else
+                            {{$rlocation["tier"]->hrs}} Hrs
+                        @endif
+                    </p>
+                </div>
+                <div class="col-span-2">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase border-b">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Item
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Qty
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($ritems as $ritem)
+                            <tr>
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                    {{$ritem->description}}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{$ritem->qty}}
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>    
+            </div>
+            <div class="flex justify-between mt-10">
+                <select wire:model="itemId" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/3">
+                    <option value="">Select Item</option>
+                    @foreach($items as $item)
+                        <option value="{{$item->id}}">{{$item->description}}</option>
+                    @endforeach
+                </select>
+                <input type="number" placeholder="Quantity" wire:model="qty" class="mx-5 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/3" min="1" value="1">
+                <button class="w-1/3 py-3 bg-lime-500 text-white rounded hover:bg-lime-600" wire:click="addItem">Add Item</button>
+            </div>
+        </div>
+        @endforeach
         <section class="{{ $confirmSection == false ? 'hidden' : 'block' }}">
             <div class="p-6 bg-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 mt-5">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -136,8 +160,12 @@
         </section>
         <div class="p-6 mt-5">
             <div class="flex justify-center">
-                @if(!is_null($companyId))
-                    <button class="w-1/6 mt-4 py-3 bg-green-500 text-white rounded hover:bg-green-600" wire:click="nextStep">{{$buttonText}}</button>
+                @if($locationSection == true)
+                    @if($rlocations != [])
+                        <button class="w-1/6 mt-4 py-3 bg-green-600 text-white rounded hover:bg-green-700" wire:click="nextStep">{{$buttonText}}</button>
+                    @endif
+                @elseif((($rType == 1) && !is_null($companyId))||($rType == 2))
+                    <button class="w-1/6 mt-4 py-3 bg-green-600 text-white rounded hover:bg-green-700" wire:click="nextStep">{{$buttonText}}</button>
                 @endif
            </div>
         </div>   
